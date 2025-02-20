0515026d41dc013220cda480d5dd597e38a34a15
walaazahranthedeveloper@gmail.com <walaazahranthedeveloper@gmail.com>
2025-02-20 23:01:49 +0200

    super eye

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -3,17 +3,42 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { AppMenuitem } from './app.menuitem';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-menu',
     standalone: true,
-    imports: [CommonModule, AppMenuitem, RouterModule],
+    imports: [CommonModule, AppMenuitem,IconFieldModule,InputIconModule,FloatLabelModule,FormsModule],
     template: `<ul class="layout-menu">
-        <ng-container *ngFor="let item of model; let i = index">
-            <li app-menuitem *ngIf="!item.separator" [item]="item" [index]="i" [root]="true"></li>
-            <li *ngIf="item.separator" class="menu-separator"></li>
-        </ng-container>
-    </ul> `
+            <p-floatlabel variant="on">
+                <p-iconfield>
+                    <p-inputicon class="pi pi-search" />
+                    <input pInputText id="in_label" autocomplete="off" />
+                </p-iconfield>
+                <label for="in_label"></label>
+            </p-floatlabel>
+            <ng-container *ngFor="let item of model; let i = index">
+
+                    <li app-menuitem *ngIf="!item.separator" [item]="item" [index]="i" [root]="true"></li>
+                    <li *ngIf="item.separator" class="menu-separator"></li>
+            </ng-container>
+            </ul> `,
+            styles:`.p-floatlabel{
+                    border: 1px solid lightgrey;
+    border-radius: 10px;
+    padding: 10px;
+
+            }
+            input:focus-visible{
+                outline:none;
+
+            }
+            input{
+                padding-left:30px;
+            }`
 })
 export class AppMenu {
     model: MenuItem[] = [];
@@ -21,137 +46,16 @@ export class AppMenu {
     ngOnInit() {
         this.model = [
             {
-                label: 'Home',
-                items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }]
-            },
-            {
-                label: 'UI Components',
+
                 items: [
-                    { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', routerLink: ['/uikit/formlayout'] },
-                    { label: 'Input', icon: 'pi pi-fw pi-check-square', routerLink: ['/uikit/input'] },
-                    { label: 'Button', icon: 'pi pi-fw pi-mobile', class: 'rotated-icon', routerLink: ['/uikit/button'] },
-                    { label: 'Table', icon: 'pi pi-fw pi-table', routerLink: ['/uikit/table'] },
-                    { label: 'List', icon: 'pi pi-fw pi-list', routerLink: ['/uikit/list'] },
-                    { label: 'Tree', icon: 'pi pi-fw pi-share-alt', routerLink: ['/uikit/tree'] },
-                    { label: 'Panel', icon: 'pi pi-fw pi-tablet', routerLink: ['/uikit/panel'] },
-                    { label: 'Overlay', icon: 'pi pi-fw pi-clone', routerLink: ['/uikit/overlay'] },
-                    { label: 'Media', icon: 'pi pi-fw pi-image', routerLink: ['/uikit/media'] },
-                    { label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'] },
-                    { label: 'Message', icon: 'pi pi-fw pi-comment', routerLink: ['/uikit/message'] },
-                    { label: 'File', icon: 'pi pi-fw pi-file', routerLink: ['/uikit/file'] },
-                    { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/uikit/charts'] },
-                    { label: 'Timeline', icon: 'pi pi-fw pi-calendar', routerLink: ['/uikit/timeline'] },
-                    { label: 'Misc', icon: 'pi pi-fw pi-circle', routerLink: ['/uikit/misc'] }
-                ]
+                { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] },
+                { label: 'Heatmaps', icon: 'pi pi-sitemap', routerLink: ['/heatmaps'] },
+                { label: 'Trajectory', icon: 'pi pi-user', routerLink: ['/trajectory'] },
+                { label: 'Cart Detection', icon: 'pi pi-envelope', routerLink: ['/cart'] },
+                { label: 'Shop Lifting', icon: 'pi pi-spinner', routerLink: ['/shop'] },
+            ]
             },
-            {
-                label: 'Pages',
-                icon: 'pi pi-fw pi-briefcase',
-                routerLink: ['/pages'],
-                items: [
-                    {
-                        label: 'Landing',
-                        icon: 'pi pi-fw pi-globe',
-                        routerLink: ['/landing']
-                    },
-                    {
-                        label: 'Auth',
-                        icon: 'pi pi-fw pi-user',
-                        items: [
-                            {
-                                label: 'Login',
-                                icon: 'pi pi-fw pi-sign-in',
-                                routerLink: ['/auth/login']
-                            },
-                            {
-                                label: 'Error',
-                                icon: 'pi pi-fw pi-times-circle',
-                                routerLink: ['/auth/error']
-                            },
-                            {
-                                label: 'Access Denied',
-                                icon: 'pi pi-fw pi-lock',
-                                routerLink: ['/auth/access']
-                            }
-                        ]
-                    },
-                    {
-                        label: 'Crud',
-                        icon: 'pi pi-fw pi-pencil',
-                        routerLink: ['/pages/crud']
-                    },
-                    {
-                        label: 'Not Found',
-                        icon: 'pi pi-fw pi-exclamation-circle',
-                        routerLink: ['/pages/notfound']
-                    },
-                    {
-                        label: 'Empty',
-                        icon: 'pi pi-fw pi-circle-off',
-                        routerLink: ['/pages/empty']
-                    }
-                ]
-            },
-            {
-                label: 'Hierarchy',
-                items: [
-                    {
-                        label: 'Submenu 1',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            {
-                                label: 'Submenu 1.1',
-                                icon: 'pi pi-fw pi-bookmark',
-                                items: [
-                                    { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
-                                    { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
-                                    { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
-                                ]
-                            },
-                            {
-                                label: 'Submenu 1.2',
-                                icon: 'pi pi-fw pi-bookmark',
-                                items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
-                            }
-                        ]
-                    },
-                    {
-                        label: 'Submenu 2',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            {
-                                label: 'Submenu 2.1',
-                                icon: 'pi pi-fw pi-bookmark',
-                                items: [
-                                    { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
-                                    { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
-                                ]
-                            },
-                            {
-                                label: 'Submenu 2.2',
-                                icon: 'pi pi-fw pi-bookmark',
-                                items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                label: 'Get Started',
-                items: [
-                    {
-                        label: 'Documentation',
-                        icon: 'pi pi-fw pi-book',
-                        routerLink: ['/documentation']
-                    },
-                    {
-                        label: 'View Source',
-                        icon: 'pi pi-fw pi-github',
-                        url: 'https://github.com/primefaces/sakai-ng',
-                        target: '_blank'
-                    }
-                ]
-            }
+
         ];
     }
 }
--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,25 +1,83 @@
 import { Component } from '@angular/core';
-import { NotificationsWidget } from './components/notificationswidget';
-import { StatsWidget } from './components/statswidget';
-import { RecentSalesWidget } from './components/recentsaleswidget';
-import { BestSellingWidget } from './components/bestsellingwidget';
-import { RevenueStreamWidget } from './components/revenuestreamwidget';
-
+import { AccordionModule } from 'primeng/accordion';
 @Component({
     selector: 'app-dashboard',
-    imports: [StatsWidget, RecentSalesWidget, BestSellingWidget, RevenueStreamWidget, NotificationsWidget],
+    imports: [ AccordionModule],
     template: `
         <div class="grid grid-cols-12 gap-8">
-            <app-stats-widget class="contents" />
-            <div class="col-span-12 xl:col-span-6">
-                <app-recent-sales-widget />
-                <app-best-selling-widget />
+            <div class="col-span-12 xl:col-span-12">
+            <p-accordion value="0">
+                <p-accordion-panel value="0">
+                    <div class="custom-header">
+                        <p-accordion-header> Select the camera</p-accordion-header>
+                    </div>
+                    <p-accordion-content >
+                        <button class="groceryBtn">Grocery Section Footage</button>
+                        <img src="https://ikancorp.com/prod1/wp-content/uploads/2022/02/Ikan-115879-Video-Footage-Cinematic-blogbanner1.jpg" width='500px'>
+                    </p-accordion-content>
+                </p-accordion-panel>
+
+
+            </p-accordion>
+
             </div>
             <div class="col-span-12 xl:col-span-6">
-                <app-revenue-stream-widget />
-                <app-notifications-widget />
+            <iframe
+                src="https://adminlte.io/themes/v3/index3.html"
+                title="Local Content"
+                width="100%"
+                height="300"
+                frameborder="0"
+                allowfullscreen="true"
+            ></iframe>
+
             </div>
+            <div class="col-span-12 xl:col-span-6">
+               <p style="text-align: center;">AI Analysis</p>
+               <article style="padding: 20px;
+    text-align: center;
+    background: rgb(81 77 77 / 50%);
+    color: #fff;
+    border-radius: 20px;">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam felis dolor, fermentum nec lacus eu, rutrum tempus risus. Fusce elit eros, elementum at finibus et, dictum sit amet felis. Duis in venenatis ligula. Vestibulum nulla lectus, ornare non lectus in, lacinia viverra leo. Nam non leo ut elit porta dapibus sit amet sed nulla. Etiam ullamcorper, leo sed luctus laoreet, ex ligula ultrices justo, ut ultricies tortor urna eu metus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean dui elit, pellentesque at purus in, hendrerit pretium magna
+                Cras congue metus a lacus tincidunt tempor. Quisque vel enim a nisi placerat tempor eu non lectus. Donec vehicula vestibulum ante et dictum. Donec ut felis massa. Aliquam nec pharetra enim, quis laoreet est. Aliquam imperdiet dapibus nisl, id mollis erat tempus eget. Morbi convallis, elit in bibendum venenatis, leo metus vulputate turpis, at cursus metus eros vitae lorem. Integer tristique, risus a blandit tempor, nulla diam ultricies lectus, vel placerat eros velit a felis.
+
+
+                </article>
+            </div>
+
+
         </div>
-    `
+    `,
+    styles:[`:host ::ng-deep {
+        --p-accordion-header-background:rgb(46, 143, 241);
+        --p-accordion-header-color: #fff;
+        --p-accordion-header-padding: 1rem;
+        --p-accordion-header-hover-background: #e9ecef;
+        --p-accordion-content-color: #fff;
+
+        .p-accordion-header {
+            background: var(--p-accordion-header-background);
+            color: var(--p-accordion-header-color);
+            padding: var(--p-accordion-header-padding);
+        }
+
+        .p-accordion-header:hover {
+            background: var(--p-accordion-header-hover-background);
+        }
+        .p-accordioncontent{
+            text-align:center;
+            button{
+                background-color:rgb(80, 162, 245);
+                color:#fff;
+                padding:10px;
+                border-radius:10px;
+                margin-top:10px;
+            }
+            img{
+                margin:50px auto;
+            }
+        }
+    }`]
 })
 export class Dashboard {}
